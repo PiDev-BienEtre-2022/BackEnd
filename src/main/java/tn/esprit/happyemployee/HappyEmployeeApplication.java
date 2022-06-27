dc1c580622e4bb2e4729dcf42b4948be03ca96f8
MeherziKH <khouloud.meherzi@esprit.tn>
2022-06-27 18:53:59 +0200

    first commit

---
+++ src/main/java/tn/esprit/happyemployee/HappyEmployeeApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.happyemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/happyemployee/HappyEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.happyemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
